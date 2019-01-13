6587423e4b1013f62dda762ac10ca56c2b5c12b4
yudeguang <84558040@qq.com>
2019-01-13 11:52:39 +0800

    andyyu

--- visitercontrol.go
+++ visitercontrol.go
@@ -8,24 +8,31 @@ import (
 
 //某单位时间内允许多少次访问
 type visitercontrol struct {
-	defaultExpiration         time.Duration       //每条访问记录需要保存的时长
-	cleanupInterval           time.Duration       //多长时间需要执行一次清除操作
-	maxVisitsNum              int                 //每个用户在相应时间段内最多允许访问的次数
-	indexes                   sync.Map            //索引：key代表用户名或IP；value代表visitorRecords中的索引位置
-	defaultVisitorRecordsSize int                 //默认的访客记录池大小，建议选用一个较大值，以减少内存分配次数
-	visitorRecords            []*circleQueueInt64 //存储用户访问记录
-	notUsedVisitorRecords     *hashset.SetInt     //对应visitorRecords中未使用的数据的索引位置
-	lock                      *sync.RWMutex       //并发锁
+	defaultExpiration          time.Duration       //每条访问记录需要保存的时长，也就是过期时间
+	cleanupInterval            time.Duration       //多长时间需要执行一次清除操作
+	maxVisitsNum               int                 //每个用户在相应时间段内最多允许访问的次数
+	indexes                    sync.Map            //索引：key代表用户名或IP；value代表visitorRecords中的索引位置
+	maximumNumberOfOnlineUsers int                 //单位时间最大用户数量，建议选用一个稍大于实际值的值，以减少内存分配次数
+	visitorRecords             []*circleQueueInt64 //存储用户访问记录
+	notUsedVisitorRecordsIndex *hashset.SetInt     //对应visitorRecords中未使用的数据的索引位置
+	lock                       *sync.RWMutex       //并发锁
 }
 
-//初始化
-func New(defaultExpiration, cleanupInterval time.Duration, maxVisitsNum, defaultVisitorRecordsSize int) *visitercontrol {
-	this := new(defaultExpiration, cleanupInterval, maxVisitsNum, defaultVisitorRecordsSize)
+/*
+初始化
+例：
+vc := visitercontrol.New(time.Minute*30, time.Second*5, 50, 1000)
+它表示:
+在30分钟内每个用户最多允许访问50次，系统每5秒针删除一次过期数据。
+并且我们预计同时在线用户数量大致在1000个左右。
+*/
+func New(defaultExpiration, cleanupInterval time.Duration, maxVisitsNum, maximumNumberOfOnlineUsers int) *visitercontrol {
+	this := new(defaultExpiration, cleanupInterval, maxVisitsNum, maximumNumberOfOnlineUsers)
 	go this.deleteExpired()
 	return this
 }
 
-func new(defaultExpiration, cleanupInterval time.Duration, maxVisitsNum, defaultVisitorRecordsSize int) *visitercontrol {
+func new(defaultExpiration, cleanupInterval time.Duration, maxVisitsNum, maximumNumberOfOnlineUsers int) *visitercontrol {
 	if cleanupInterval > defaultExpiration {
 		panic("每次清除访问记录的时间间隔(cleanupInterval)必须小于待统计数据时间段(defaultExpiration)")
 	}
@@ -34,20 +41,20 @@ func new(defaultExpiration, cleanupInterval time.Duration, maxVisitsNum, default
 	l.defaultExpiration = defaultExpiration
 	l.cleanupInterval = cleanupInterval
 	l.maxVisitsNum = maxVisitsNum
-	l.defaultVisitorRecordsSize = defaultVisitorRecordsSize
-	l.notUsedVisitorRecords = hashset.NewInt()
+	l.maximumNumberOfOnlineUsers = maximumNumberOfOnlineUsers
+	l.notUsedVisitorRecordsIndex = hashset.NewInt()
 	l.lock = &lock
 	//初始化缓存池，减少内存分配，提升性能
-	l.visitorRecords = make([]*circleQueueInt64, 0, l.defaultVisitorRecordsSize)
+	l.visitorRecords = make([]*circleQueueInt64, 0, l.maximumNumberOfOnlineUsers)
 	for i := range l.visitorRecords {
 		l.visitorRecords[i] = newCircleQueueInt64(l.maxVisitsNum)
-		l.notUsedVisitorRecords.Add(i)
+		l.notUsedVisitorRecordsIndex.Add(i)
 	}
 	return &l
 
 }
 
-//是否允许访问
+//是否允许访问,允许访问则往访问记录中加入一条访问记录
 func (this *visitercontrol) AllowVisit(key interface{}) bool {
 	return this.add(key) == nil
 }
@@ -63,10 +70,10 @@ func (this *visitercontrol) add(key interface{}) (err error) {
 		this.lock.RLock()
 		defer this.lock.RUnlock()
 		//有未使用的缓存时
-		if this.notUsedVisitorRecords.Size() > 0 {
-			for index := range this.notUsedVisitorRecords.Items {
+		if this.notUsedVisitorRecordsIndex.Size() > 0 {
+			for index := range this.notUsedVisitorRecordsIndex.Items {
 				this.visitorRecords[index].Push(time.Now().Add(this.defaultExpiration).UnixNano())
-				this.notUsedVisitorRecords.Remove(index)
+				this.notUsedVisitorRecordsIndex.Remove(index)
 				//下标索引位置
 				this.indexes.Store(key, index)
 				break
@@ -84,7 +91,7 @@ func (this *visitercontrol) add(key interface{}) (err error) {
 	}
 }
 
-//基于用户的频率控制
+//删除过期数据
 func (this *visitercontrol) deleteExpired() {
 	finished := true
 	for range time.Tick(this.cleanupInterval) {
@@ -97,7 +104,7 @@ func (this *visitercontrol) deleteExpired() {
 	}
 }
 
-//删除过期数据
+//在特定时间间隔内执行一次删除过期数据操作
 func (this *visitercontrol) deleteExpiredOnce() {
 	this.indexes.Range(func(k, v interface{}) bool {
 		index := v.(int)
@@ -107,13 +114,13 @@ func (this *visitercontrol) deleteExpiredOnce() {
 			this.lock.Lock()
 			defer this.lock.Unlock()
 			this.indexes.Delete(k)
-			this.notUsedVisitorRecords.Add(index)
+			this.notUsedVisitorRecordsIndex.Add(index)
 		}
 		return true
 	})
 }
 
-//出现峰值之后，回收访问数据
+//出现峰值之后，回收访问数据，减少内存占用
 func (this *visitercontrol) gc() {
 	this.lock.Lock()
 	defer this.lock.Unlock()
